ab1ee6f3bf12662f88d5441d3b9655419d555c8f
MarioJC <mario.rol@gmail.com>
2019-09-25 09:32:34 +0200

    Conditional expressions

--- src/Game.kt
+++ src/Game.kt
@@ -6,22 +6,23 @@ fun main() {
 
     // Aura
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    if (auraVisible)
-        println("GREEN")
-    else
-        println("NONE")
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
 
-    if (healthPoints == 100)
-        println(name + " is in excellent condition!")
+    val healthStatus = if (healthPoints == 100)
+        "is in excellent condition!"
     else if (healthPoints >= 90)
-        println(name + " has a few scratches.")
+        "has a few scratches."
     else if (healthPoints >= 75)
         if (isBlessed)
-            println(name + " has some minor wounds but is healing quickly!")
+            "has some minor wounds but is healing quickly!"
         else
-            println(name + " has some minor wounds")
+            "has some minor wounds."
     else if (healthPoints >= 15)
-        println(name + " looks pretty hurt.")
+        "looks pretty hurt."
     else
-        println(name + " is in awful condition!")
+        "is in awful condition!"
+
+    // Player status
+    println(name + " " + healthStatus)
 }
