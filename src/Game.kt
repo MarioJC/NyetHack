70859f8fd2905c734cbd87f264f35306e8e97af5
MarioJC <mario.rol@gmail.com>
2019-09-26 11:51:02 +0200

    Ranges operator

--- src/Game.kt
+++ src/Game.kt
@@ -11,14 +11,14 @@ fun main() {
 
     val healthStatus = if (healthPoints == 100)
         "is in excellent condition!"
-    else if (healthPoints >= 90)
+    else if (healthPoints in 90..99)
         "has a few scratches."
-    else if (healthPoints >= 75)
+    else if (healthPoints in 75..89)
         if (isBlessed)
             "has some minor wounds but is healing quickly!"
         else
             "has some minor wounds."
-    else if (healthPoints >= 15)
+    else if (healthPoints in 15..74)
         "looks pretty hurt."
     else
         "is in awful condition!"
