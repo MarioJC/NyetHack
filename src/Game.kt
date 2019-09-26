276a5cfa3fad3e2df7e9238bb5c087b856e145c3
MarioJC <mario.rol@gmail.com>
2019-09-26 13:41:12 +0200

    when expression

--- src/Game.kt
+++ src/Game.kt
@@ -9,19 +9,14 @@ fun main() {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100)
-        "is in excellent condition!"
-    else if (healthPoints in 90..99)
-        "has a few scratches."
-    else if (healthPoints in 75..89)
-        if (isBlessed)
-            "has some minor wounds but is healing quickly!"
-        else
-            "has some minor wounds."
-    else if (healthPoints in 15..74)
-        "looks pretty hurt."
-    else
-        "is in awful condition!"
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) "has some minor wounds but is healing quickly!"
+                     else "has some minor wounds."
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
 
     // Player status
     println(name + " " + healthStatus)
