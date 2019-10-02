e5f5ba6d982522831f5ccc060867ef99929df526
MarioJC <mario.rol@gmail.com>
2019-10-02 15:50:07 +0200

    Single-Expression Functions

--- src/Game.kt
+++ src/Game.kt
@@ -1,23 +1,45 @@
 fun main() {
     val name = "Madrigal"
-    var healthPoints = 89
+    val healthPoints = 89
     val isBlessed = true
     val isImmortal = false
 
     // Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
 
-    val healthStatus = when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) "has some minor wounds but is healing quickly!"
-                     else "has some minor wounds."
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
     // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireBall()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
     println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$name $healthStatus")
 }
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun castFireBall(numFireBalls: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireBalls)")
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
+    when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) "has some minor wounds but is healing quickly!"
+        else "has some minor wounds."
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
