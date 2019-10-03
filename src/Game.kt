f4a26570f3de0dae7d622cd884ccb7cee9f4fc03
MarioJC <mario.rol@gmail.com>
2019-10-03 12:16:27 +0200

    Challenges: Fireball Inebration Level && Inebration Status

--- src/Game.kt
+++ src/Game.kt
@@ -6,32 +6,35 @@ fun main() {
 
     // Aura
     val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
+    var fireBallsCast : Int = 0
+    fireBallsCast += castFireBall(9)
+    fireBallsCast += castFireBall()
+
     // Player status
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-
-    castFireBall()
-
-    printPlayerStatus(name = "Mario", healthStatus = "rocks!", auraColor = "GOLD", isBlessed = true)
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus, fireBallsCast)
 }
 
 private fun printPlayerStatus(
     auraColor: String,
     isBlessed: Boolean,
     name: String,
-    healthStatus: String
+    healthStatus: String,
+    numFireballsCast: Int
 ) {
     println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$name $healthStatus")
+    println("A total of $numFireballsCast fireballs were cast. $name is ${inebrationStatus(numFireballsCast)}!")
 }
 
 private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean) =
     if (isBlessed && healthPoints > 50 || isImmortal) "GREEN" else "NONE"
 
-private fun castFireBall(numFireBalls: Int = 2) =
+private fun castFireBall(numFireBalls: Int = 2) : Int {
     println("A glass of Fireball springs into existence. (x$numFireBalls)")
+    return numFireBalls
+}
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
     when (healthPoints) {
@@ -41,4 +44,14 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
         else "has some minor wounds."
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
+    }
+
+private fun inebrationStatus(inebrationValue: Int) =
+    when (inebrationValue) {
+        in 1..10 -> "tipsy"
+        in 11..20 -> "sloshed"
+        in 21..30 -> "soused"
+        in 31..40 -> "stewed"
+        in 41..50 -> "t0aSt3d"
+        else -> "dead"
     }
