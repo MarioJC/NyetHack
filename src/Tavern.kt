4d95b513762aa6677fd2700812fa422c4ca8f97e
MarioJC <mario.rol@gmail.com>
2019-10-05 15:49:49 +0200

    The null coalescing operator ?:

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -9,5 +9,9 @@ fun main() {
         println("I can't do that without crashing - beverage was null!")
     }
 
-    println(beverage)
+//    beverage = null
+
+    // als beverage null is krijgt beverageServed de fallback waarde Buttered Ale
+    val beverageServed = beverage ?: "Buttered Ale"
+    println(beverageServed)
 }
