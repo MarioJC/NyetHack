d9dd1ff7e71c1fcb13b3d7485677990780a769dc
MarioJC <mario.rol@gmail.com>
2019-10-05 15:40:04 +0200

    Checking nullness with if..else construct

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,10 +1,13 @@
 fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "Buttered Ale"
-        }
+    var beverage = readLine()
+
+    if (beverage != null) {
+        // beverage wordt gesmartcast van String? naar String
+        // omdat in deze branch beverage gegarandeerd niet null is
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
     }
+
     println(beverage)
 }
