52746ec3e4ea7661f03ace65d7711b75c38fcd7c
MarioJC <mario.rol@gmail.com>
2019-10-05 17:25:07 +0200

    Strings

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,17 +1,36 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
-    var beverage = readLine()
-
-    if (beverage != null) {
-        // beverage wordt gesmartcast van String? naar String
-        // omdat in deze branch beverage gegarandeerd niet null is
-        beverage = beverage.capitalize()
-    } else {
-        println("I can't do that without crashing - beverage was null!")
-    }
-
-//    beverage = null
-
-    // als beverage null is krijgt beverageServed de fallback waarde Buttered Ale
-    val beverageServed = beverage ?: "Buttered Ale"
-    println(beverageServed)
+    placeOrder("shandy,Dragon's Breath,5.91")
+    println("---")
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "Madrigal exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println(phrase)
+}
+
+private fun toDragonSpeak(phrase: String) =
+        phrase.replace(Regex("[aeiou]")) {
+            when (it.value) {
+                "a" -> "4"
+                "e" -> "3"
+                "i" -> "1"
+                "o" -> "0"
+//                "u" -> "|_|"
+                else -> it.value
+            }
+        }
