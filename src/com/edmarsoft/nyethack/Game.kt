9f3d09ea836e477e014c9fc0b8ed871fd16b32f5
MarioJC <mario.rol@gmail.com>
2019-10-17 17:51:48 +0200

    Game object
    Encapsulate existing code in new Game object to use as game loop.
    Add some basic command processing functionality.
    Just the basic outline, does NOT recognize valid commands (yet!)

--- src/com/edmarsoft/nyethack/Game.kt
+++ src/com/edmarsoft/nyethack/Game.kt
@@ -1,17 +1,46 @@
 package com.edmarsoft.nyethack
 
 fun main() {
-    val player = Player("Madrigal")
-    player.castFireBall()
-
-    var currentRoom: Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    printPlayerStatus(player)
+    Game.play()
 }
 
-private fun printPlayerStatus(player: Player) {
-    println("(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()} (HP: ${player.healthPoints})")
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom: Room = TownSquare()
+
+    init {
+        println("Welcome, adventurer.")
+        player.castFireBall()
+    }
+
+    fun play() {
+        while (true) { // Nyethack game loop
+
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println(GameInput(readLine()).processCommand())
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println("(Aura: ${player.auraColor()}) (Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()} (HP: ${player.healthPoints})")
+    }
+
+    private class GameInput(arg: String?) {
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1) { "" }
+
+        fun processCommand() = when(command.toLowerCase()) {
+            // TODO: add all valid commands
+            else -> commandNotFound()
+        }
+
+        private fun commandNotFound() = "I'm not quite what you're trying to do!"
+    }
 }
