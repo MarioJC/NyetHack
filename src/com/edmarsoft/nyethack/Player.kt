98876778b0fe61c4401f5657eeb26b7f745bf099
MarioJC <mario.rol@gmail.com>
2019-10-19 14:37:36 +0200

    Introducing Monsters and Goblins
    Topics: interfaces, default implementations, abstract classes

--- src/com/edmarsoft/nyethack/Player.kt
+++ src/com/edmarsoft/nyethack/Player.kt
@@ -3,9 +3,23 @@ package com.edmarsoft.nyethack
 import java.io.File
 
 class Player (_name: String,
-              var healthPoints: Int = 100,
+              override var healthPoints: Int = 100,
               val isBlessed: Boolean,
-              private val isImmortal: Boolean){
+              private val isImmortal: Boolean) : Fightable {
+    override val diceCount = 3
+    override val diceSides = 6
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = if (isBlessed) {
+            damageRoll * 2
+        } else {
+            damageRoll
+        }
+
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+
     var name = _name
         get() = "${field.capitalize()} of $hometown"
         private set(value) {
